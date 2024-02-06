cf88960eb5b975dfe55850fa282baf99028291b6
aniket <aniketp3898@gmail.com>
2024-02-06 15:50:08 +0530

    tested the api is working or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 const App = () => {
-  return (
-    <div className="App">
-      App
-    </div>
-  )
-}
+  
+  const dispatch=useDispatch();
+  const {url} =useSelector((state)=>state.home)
 
-export default App
+  useEffect(()=>{
+    apiTesting()
+  },[])
+
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res))
+    });
+  };
+
+  return <div className="App">App
+   {url?.total_pages}
+  </div>;
+};
+
+export default App;
